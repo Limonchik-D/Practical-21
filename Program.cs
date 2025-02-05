08da150d52e4e415f8a47d9edb1cbecf6f4188fa
Limon <Limon@DAVID>
2025-02-05 09:24:06 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time();
+            time.hour = 9;
+            time.minutes = 30;
+            time.seconds = 90;
+
+            Time time1 = new Time(12, 30, 45); //Явная инициализация
+            Console.WriteLine(time1);
+
+            Console.Write("Введите кол-во часов: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во минут: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во секунд: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Time time2 = new Time(h, m, s);
+            Console.WriteLine(time2);
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_21
+{
+    internal class Time
+    {
+        public int hour;
+        public int minutes;
+        public int seconds;
+        public Time()
+        {
+            hour = 0;
+            minutes = 0;
+        }
+        public Time(int hour)
+        {
+            this.hour = hour;
+            minutes = 0;
+        }
+        public Time (int hour, int minutes) : this(hour)
+        {
+            this.hour = hour;
+            this.minutes = minutes;
+        }
+        public Time(int hour, int minutes, int seconds) : this(hour, minutes)
+        {
+            this.seconds = seconds;
+        }
+
+        public int SumTime()
+        {
+            return hour * 60 + minutes; // количество минут
+        }
+        public int SumSeconds()
+        {
+            return hour * 3600 + minutes * 60 + seconds; // количество секунд
+        }
+        public override string ToString()
+        { 
+            return $"Время: {hour} Ч {minutes} Мин {seconds} сек, всего минут: {SumTime()}, всего секунд: {SumSeconds()}";
+        }
+    }
+}
